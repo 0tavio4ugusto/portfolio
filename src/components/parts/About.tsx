b186a39a5c65580cb1c56a9eaa494c2bc4f72c42
Otavio Augusto <voiglet@gmail.com>
2023-07-13 19:08:24 -0300

    Update About.tsx

--- src/components/parts/About.tsx
+++ src/components/parts/About.tsx
@@ -7,15 +7,13 @@ const About = () => {
       <div className="flex flex-row justify-evenly">
       <img className=" scale-105 " src={eu} alt="" />
       <div className="w-1/4 text-justify self-center scale-125">
-        <p className=" text-purple-300 text-5xl font-black text-center"> - SOBRE MIM - </p>
-        <p className="font-black text-gray-200 mt-12">
-          Olá, me chamo Otavio Augusto, tenho 21 anos atualmente estou dedicando 100% do meu tempo em melhorar minhas habilidades em desenvolvimento, Com foco em Web usando <span className="text-teal-300">Tailwind </span>,
+        <p className=" text-purple-300 text-5xl font-black text-center self-center"> - SOBRE MIM - </p>
+        <p className="font-black text-gray-200 mt-12 w-11/12">
+        Olá, me chamo Otavio Augusto, tenho 21 anos e atualmente, estou dedicando 100% do meu tempo para melhorar minhas habilidades em desenvolvimento, com foco em Web usando <span className="text-teal-300">Tailwind </span>,
           <span className="text-blue-400">TypeScript </span> /
           <span className="text-yellow-300"> JavaScript</span> e
-          <span className="text-cyan-300"> React</span>.    
-        </p>
-        <p className="font-black text-gray-200">
-          Busco constante evolução em minhas habilidades e conhecimentos, tanto em diferentes areas da tecnologia quanto dentro da esfera de front-end!
+          <span className="text-cyan-300"> React</span>.
+          Busco constante evolução em minhas habilidades e conhecimentos, tanto em diferentes áreas da tecnologia quanto dentro da esfera de front-end! Por último, mas não menos importante, tenho o anseio profissional de poder trabalhar na área com uma equipe, para que eu possa aprender não apenas com a prática, mas também com os diversos conhecimentos e experiências dos meus colegas de profissão!  
         </p>
       </div>
       </div>
